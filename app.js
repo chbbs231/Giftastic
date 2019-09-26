159b339c167ca42829c4031fbf1734defc337686
chbbs231 <atrobles231@gmail.com>
2019-09-26 00:01:46 -0700

    some changes

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
+let gif =[]
+
 document.addEventListener(`click`, event => {
     if(event.target.className ==='cartoon'){
         let cartoon = event.target.dataset.cartoon
         let url = `https://api.giphy.com/v1/gifs/search?api_key=zvKblsxu8EHnNiofZHPJ73ew4MuRsIIY&q=cartoons&limit=10&offset=0&rating=PG-13&lang=en`
-   fetch(url)
-        .then(r => r.json())
+   fetch(`https://api.giphy.com/v1/gifs/search?api_key=zvKblsxu8EHnNiofZHPJ73ew4MuRsIIY&q=cartoons&limit=10&offset=0&rating=PG-13&lang=en`)
+       .then(result => result.json())
         .then(gifs => {
            document.getElementById(`gpics`).innerHTML = ``
             gifs.data.forEach(gif => {
                 let gifElem = document.createElement(`img`)
-                gifElem.src =gif.image.original_still.url
+                gifElem.src = gif.image.original.still.url
                 gitElem.dataset.still = gif.url
                 gifElem.src = gif.image.original.url
                 document.getElementById(`gpics`).append(gifElem)
                 
             })
         })
-    } else if (event.target.className === `gif`){
+    }else if (event.target.className === `gif`){
         if(event.target.isanimated === `true`){
             event.target.src =event.target.dataset.still
             event.target.isanimated = `false`
